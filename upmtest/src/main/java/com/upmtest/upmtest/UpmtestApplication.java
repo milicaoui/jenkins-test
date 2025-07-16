290fa54cbbfece5b28bf3c85eacf3b53983e9355
vumilicaamm <milica.vucinic@code2.tech>
2025-07-16 10:14:22 +0200

    added spring boot test application

---
+++ upmtest/src/main/java/com/upmtest/upmtest/UpmtestApplication.java
@@ -0,0 +1,13 @@
+package com.upmtest.upmtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpmtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpmtestApplication.class, args);
+	}
+
+}
