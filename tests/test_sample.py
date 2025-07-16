fddf45f8191043a6c865803d2a3fb15b66548859
vumilicaamm <milica.vucinic@code2.tech>
2025-07-16 08:28:19 +0200

    added another test

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,2 +1,11 @@
-def test_always_passes():
-    assert True
+import requests
+
+
+# def test_always_passes():
+#     assert True
+
+
+def test_hello_endpoint():
+    response = requests.get("http://localhost:8081/api/hello")
+    assert response.status_code == 200
+    assert response.text == "Hello from Spring Boot!"
